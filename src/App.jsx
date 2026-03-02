d93e11a3b49050ab981f76c700389ed85aceeb19
mario <mario_reyes505@hotmail.com>
2026-03-01 22:28:29 -0500

    update css for flipped class, create Card.jsx , GameHeader.jsx, add them to App.jsx. Add state to handle clickking on card once

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
+import Card from "./components/Card"
+import GameHeader from "./components/GameHeader"
+import { useEffect, useState } from "react";
+
+const cardValues = [
+  '🍌','🍉','🍎','🍓','🥜','🍍','🍐','🍋',
+  '🍌','🍉','🍎','🍓','🥜','🍍','🍐','🍋'
+]
 
 function App() {
 
+  const [cards, setCards] = useState([]);
+    
+  function initializeGame(){
+      // shuffle the cards
+
+      const finalCards = cardValues.map((value, index) => (
+        {
+            id: index, 
+            value,
+            isFlipped: false,
+            isMatched: false
+        }
+      ));
+      setCards(finalCards);
+  }
+
+  useEffect(()=> {
+    initializeGame();
+  }, []);
+
+  function handleCardClick (card) {
+    // dont allow clicking if card is already flipped or matched
+    if (card.isFlipped || card.isMatched) {
+      return;
+    }
+
+    // update card flipped state
+    const newCards = cards.map((c) => {
+      if (c.id === card.id){
+        return {...c, isFlipped : true};
+      } else {
+        return c;
+      }
+    });
+
+    setCards(newCards);
+  }
+
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="app">
+      <GameHeader score={0} moves={3}/>
+
+      <div className="cards-grid">
+        {cards.map((card, index)=>(
+          <Card key={index} card={card} onClick={handleCardClick}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+
+
+export default function Card({card , onClick}) {
+
+    return (                
+        <div className={`card ${card.isFlipped ? "flipped": ""}`} 
+            onClick={ ()=> onClick(card)}>
+            <div className="card-front">?</div>
+            <div className="card-back">{card.value}</div>
+        </div>
+    );
+}
